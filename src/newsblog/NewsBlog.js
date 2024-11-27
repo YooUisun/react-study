d4c55e5e5023389387431561f8c1a417d011eb9b
YooUisun <yoouisun33@naver.com>
2024-11-27 16:24:37 +0900

    좋아요 갯수전달, 삭제기능추가

--- src/newsblog/NewsBlog.js
+++ src/newsblog/NewsBlog.js
@@ -7,18 +7,27 @@ function NewsBlog() {
     //javascript 코드
     let title = 'React Study';
 
-    let [news1, setNews1] = useState('오늘의 뉴스');
-    let [news2, setNews2] = useState('어제의 뉴스');
-    let [news3, setNews3] = useState('내일의 뉴스');
-
-    let [news, setNews] = useState(['오늘의 뉴스', '어제의 뉴스', '내일의 뉴스']);
-    //글 제목 3개
+    // 아래 3개의 state 변수를 하나의 news 배열로 통합
+    let [news, setNews] = useState(['오늘의 뉴스', '어제의 뉴스', '내일의 뉴스']); 
+    // 좋아요 갯수 정보를 하나의 likeCount 배열로 통합
     let [likeCount, setLikeCount] = useState([0, 0, 0]);
-    //좋아요 갯수 3개
-
+    
     let [modalFlag, setModalFlag] = useState(false);
 
     let [selectedTitle, setSelectedTitle] = useState('');
+    let [selectedLikeCount, setSelectedLikeCount] = useState(0);
+    let [inputText, setInputText] = useState('');
+
+    // 뉴스 항목 삭제 기능을 위한 deleteNews 함수 추가
+    const deleteNews = (index) => {
+        let temp = [...news];
+        temp.splice(index, 1);
+        setNews(temp);
+
+        let tempLike = [...likeCount];
+        tempLike.splice(index, 1);
+        setLikeCount(tempLike);
+    };
 
     return (
         <div>
@@ -31,30 +40,62 @@ function NewsBlog() {
                 news.map((item, index) => {
                     return (
                         <div className="post-list">
-                            <h4 onClick={() => {    //제목 클릭
-                                setModalFlag(!modalFlag);   //모달창이 on/off 전환
-                                //어떤 뉴스를 눌렀는지! --> 저장
-                                setSelectedTitle('선택한 제목');
-                            }}>{news[index]} <span onClick={(event) => {
-                                event.stopPropagation(); //이벤트 추가 전달 stop
-                                let temp = [...likeCount];
-                                temp[index]++;
-                                setLikeCount(temp);
-                            }}>♥</span> {likeCount[index]} </h4>
+                            <h4 onClick={() => { 
+                                setModalFlag(!modalFlag);
+                                setSelectedTitle(item);
+                                setSelectedLikeCount(likeCount[index]);
+                            }}>
+                                {item} <span onClick={(event) => {
+                                    event.stopPropagation();
+                                    let temp = [...likeCount];
+                                    temp[index]++;
+                                    setLikeCount(temp);
+                                }}>❤</span> {likeCount[index]}
+                            </h4>
                             <p>내용 무</p>
+                            <button onClick={()=>{
+                                //누른 내용 데이터가 삭제
+
+                                //index 활용
+                                //splice 활용
+                                let temp = [...news];
+                                temp.splice(index, 1);
+                                setNews(temp);
+
+                                likeCount.splice(index, 1);
+
+
+                            }}>삭제</button>
+
                         </div>
                     )
                 })
             }
 
-            <button onClick={() => {
-                let temp = [...news];
-                temp[0] = 'Today News';
-                setNews(temp);
-            }}>제목 변경</button>
+            <div>
+                <input type="text" id="input_news" value={inputText} onChange={(event)=>{
+                    setInputText(event.target.value);
+                }}/>
+                <button onClick={()=>{
+                    if( inputText.trim() === ''){
+                        alert('값을 입력하세요');
+                        return;
+                    }
+
+                    let temp = [...news];
+                    temp.push(inputText);
+                    setNews(temp);
+                    
+                    likeCount.push(0);
+
+                    setInputText('');
+                }}>발행</button>
+            </div>
+
             {
-                modalFlag == true ? <Modal title={selectedTitle}news={news} setNews={setNews} bgColor={'lightcyan'} /> : null
+                modalFlag && <Modal title={selectedTitle} likeCount={selectedLikeCount} news={news} setNews={setNews} bgColor={'lightcyan'} />
             }
+
         </div>
     )
 }
